4653fd2edd36ddbc129031fb25273af164bf9705
automationtrainingcenter <atcsurya@gmail.com>
2018-11-18 11:29:16 +0530

    added browser setup and close methods in base class

--- keximbank/src/test/java/in/srssprojects/keximbank/BaseClass.java
+++ keximbank/src/test/java/in/srssprojects/keximbank/BaseClass.java
@@ -1,5 +1,44 @@
 package in.srssprojects.keximbank;
 
-public class BaseClass {
+import java.io.File;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseClass {
+	WebDriver driver;
+	/**
+	 * returns path of the fileName which is in the folderName
+	 * folder must be inside the project root folder
+	 * and file name must contain extensions also
+	 * @param folderName
+	 * @param fileName
+	 * @return
+	 *
+	 */
+	public String getFilePath(String folderName, String fileName) {
+		return System.getProperty("user.dir")+File.separator+folderName+File.separator+fileName;
+	}
+	
+	public void launchBrowser(BrowserName name, String url) {
+		switch (name) {
+		case chrome:
+			System.setProperty("webdriver.chrome.driver",getFilePath("resources", "chromedriver.exe"));
+			driver = new ChromeDriver();
+			break;
+		case firefox:
+			System.setProperty("webdriver.gecko.driver",getFilePath("resources", "geckodriver.exe"));
+			driver = new FirefoxDriver();
+			break;
+		default:
+			throw new RuntimeException("browser name is invalid");
+		}
+		driver.get(url);
+		driver.manage().window().maximize();
+	}
+	
+	public void closeBrowser() {
+		driver.close();
+	}
 }
---
+++ keximbank/src/test/java/in/srssprojects/keximbank/BrowserName.java
@@ -0,0 +1,6 @@
+package in.srssprojects.keximbank;
+
+public enum BrowserName {
+	chrome, firefox;
+
+}
